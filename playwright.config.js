7bb08023bb4fdd66f9424af180419ed2558a7343
Anioska Assist <anioska@assist-365.com>
2026-01-19 14:59:31 -0300

    Actualiza configuración de Playwright y elimina test obsoleto

--- playwright.config.js
+++ playwright.config.js
@@ -28,9 +28,9 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-      headless: false,
+      headless: true,
       launchOptions: {
-        slowMo: 500,
+       // slowMo: 500,
       },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
